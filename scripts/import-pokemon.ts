4879f53c1f7ca3ae954a245494f4b222bcc03645
twist <s.bonnet230906@gmail.com>
2026-05-31 17:46:20 +0200

    🛠️ Feat & evol : Amélioration Schéma + Récupération nameFr + currentSprite ( 1er jeux disponible )

--- scripts/import-pokemon.ts
+++ scripts/import-pokemon.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config'
 import { prisma } from '../server/utils/prisma'
+import { buildPokemonSpriteUrl } from '../server/utils/sprite'
 
 async function fetchPokemonApi(id: number) {
     return await fetch(
@@ -7,15 +8,28 @@ async function fetchPokemonApi(id: number) {
     ).then(res => res.json())
 }
 
+async function fetchPokemonSpecies(id: number) {
+    return await fetch(
+        `https://pokeapi.co/api/v2/pokemon-species/${id}`,
+    ).then(res => res.json())
+}
+
 function delay(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms))
 }
 
+// TODO segmenté seed : / générations
 async function main() {
-    for (let id = 1; id <= 1025; id++) {
+    for (let id = 1; id <= 10; id++) {
         console.log(`Importing #${id}`)
 
         const pokemon = await fetchPokemonApi(id)
+        const species = await fetchPokemonSpecies(id)
+
+        const nameFr =
+            species.names.find((n: any) => n.language.name === 'fr')?.name ??
+            pokemon.name
+
         await delay(150)
 
         const versions = pokemon.sprites?.versions ?? {}
@@ -24,6 +38,14 @@ async function main() {
             generation: string
             game: string
         }[] = []
+
+        const firstGeneration = Object.keys(versions)[0]
+        if (!firstGeneration) throw new Error("No generation found")
+
+        const firstGame = Object.keys(versions[firstGeneration] ?? {})[0]
+        if (!firstGame) throw new Error("No game found")
+
+        const currentSprite: string = buildPokemonSpriteUrl(pokemon.id, firstGeneration, firstGame, false)
 
         for (const generation in versions) {
             const generationGames = versions[generation]
@@ -42,12 +64,14 @@ async function main() {
             },
             update: {
                 availableGames: games,
+                currentSprite,
             },
             create: {
                 pokeNumber: pokemon.id,
                 nameEn: pokemon.name,
-                nameFr: pokemon.name,
+                nameFr: nameFr,
                 availableGames: games,
+                currentSprite: currentSprite,
             },
         })
     }
