288dedac5b22e6022f4529ef84747712fdc4271f
twist <s.bonnet230906@gmail.com>
2026-06-06 21:30:20 +0200

    🖼️️ Feat & Evol : Création components et vue Individuel pokemon
    -Refactor app.vue
    -Page profil
    -Pokemon card

---
+++ components/logout.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click="logout">
+    Logout
+  </button>
+</template>
+
+<style scoped>
+
+</style>
+
+<script setup lang="ts">
+const token = useCookie('token')
+
+async function logout() {
+  token.value = null
+  await navigateTo('/')
+}
+</script>
---
+++ pages/pokedex/[id].vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ pokemon?.nameFr }}
+    <img :src="pokemon?.currentSprite" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script setup lang="ts">
+const route = useRoute();
+const id = route.params.id;
+const { data: pokemon } = await useFetch(`/api/pokemon/${id}`)
+</script>
