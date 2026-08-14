0db29012a6dafd80e7b5a54490362f613841f8f4
twist <s.bonnet230906@gmail.com>
2026-08-14 18:15:06 +0200

    evol : Amélioration interface changement affichage cartes, affichage des cartes, disposition recherche

--- components/pokemonCard.vue
+++ components/pokemonCard.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="flex flex-col items-center w-20">
-    <!-- Update logo en custom one-->
-    <img v-if="!imageLoader" src="https://i.gifer.com/4OKl.gif">
-    <img :src="pokemon.currentSprite ?? undefined" loading="lazy" @load="imageLoader = true"/>
-    <p>{{ pokemon.nameFr }}</p>
+  <div class="w-36 flex flex-col items-center bg-white border border-slate-200 rounded-md p-2 hover:border-slate-400 transition-colors">
+    <span class="text-[10px] text-slate-400 self-start">#{{ pokemon.pokeNumber }}</span>
+    <div v-if="!imageLoader" class="w-full aspect-square bg-slate-100 rounded">
+      <img src="https://i.gifer.com/4OKl.gif">
+    </div>
+    <img ref="spriteEl" :src="pokemon.currentSprite ?? undefined" class="w-full aspect-square object-contain" loading="lazy" @load="onSpriteLoaded"/>
+    <p class="mt-1 text-xs w-full text-center truncate">{{ pokemon.nameFr }}</p>
   </div>
 </template>
 
@@ -22,4 +24,13 @@ const props = defineProps<{
 }>()
 
 const imageLoader = ref(false)
+const spriteEl = ref<HTMLImageElement>()
+
+function onSpriteLoaded() {
+  imageLoader.value = true
+}
+
+onMounted(() => {
+  if (spriteEl.value?.complete) imageLoader.value = true
+})
 </script>
