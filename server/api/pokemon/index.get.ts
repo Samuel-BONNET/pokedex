4948e953b96b743e7d4ae802940aa39833d1dc25
twist <s.bonnet230906@gmail.com>
2026-08-19 12:58:14 +0200

    🪝 feat: Update API
    +Suppression statut en cas d'exactitude avec celle par defaut

--- server/api/pokemon/index.get.ts
+++ server/api/pokemon/index.get.ts
@@ -6,13 +6,15 @@ export default defineEventHandler(async (event) => {
         include: {
             statuts: {
                 where: { idUser: { in: [idUser, 0] } },
-                select: { currentSprite: true, idUser: true },
+                select: { currentSprite: true, idUser: true, isOwned: true, isShiny: true },
             },
         },
     })
     return pokemons.map(p => ({
         ...p,
         currentSprite: p.statuts.find(s => s.idUser === idUser)?.currentSprite ?? p.statuts.find(s => s.idUser === 0)?.currentSprite ?? null,
+        isOwned: p.statuts.find(s => s.idUser === idUser)?.isOwned ?? false,
+        isShiny: p.statuts.find(s => s.idUser === idUser)?.isShiny ?? false,
         statuts: undefined,
     }))
 })
