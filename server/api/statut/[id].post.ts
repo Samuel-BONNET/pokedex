5103e13709accc5335a70436fb7cbd6684adbe80
twist <s.bonnet230906@gmail.com>
2026-08-20 18:39:29 +0200

    🧹 Nettoyage code mort

--- server/api/statut/[id].post.ts
+++ server/api/statut/[id].post.ts
@@ -3,17 +3,12 @@ export default defineEventHandler(async (event) => {
     const body = await readBody(event)
     const idUser = body.idUser ?? 1
     if (idUser === 0) return
-    const currentSprite = body.currentSprite
-    const gameName = body.gameName
     const isOwned = body.isOwned
     const isShiny = body.isShiny
 
     const defaultStatut = await prisma.statut.findUnique({
         where: { idPokemon_idUser: { idPokemon, idUser: 0} }
     })
-
-    const game = gameName ? await prisma.game.findUnique({ where: { nameEn: gameName } }): null
-    const idGameProvenance = game?.id ?? 1
 
     const result = await prisma.statut.upsert({
         where: {
@@ -25,24 +20,20 @@ export default defineEventHandler(async (event) => {
         update: {
             idPokemon,
             idUser,
-            currentSprite,
-            idGameProvenance,
             isOwned,
             isShiny,
         },
         create: {
             idPokemon,
             idUser,
-            currentSprite: currentSprite ?? null,
-            idGameProvenance,
             isOwned: isOwned ?? false,
             isShiny: isShiny ?? false,
         },
     })
 
-    if (defaultStatut && defaultStatut.currentSprite !== null && result.isOwned === defaultStatut.isOwned && defaultStatut.isShiny === result.isShiny) {
+    if (defaultStatut && result.isOwned === defaultStatut.isOwned && defaultStatut.isShiny === result.isShiny) {
         await prisma.statut.delete({
-            where: { idPokemon_idUser: { idPokemon, idUser}}
+            where: { idPokemon_idUser: { idPokemon, idUser} }
         })
         return { deleted: true }
     }
