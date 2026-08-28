d8b9136aeddeb5b0e872a0ad0c19e85a6d4b9b47
twist <s.bonnet230906@gmail.com>
2026-08-28 11:37:35 +0200

    🔨 feat & evol : Upgrade Toolbar
    Import Lucide
    Ajout :
    - mode de selection d'un point A à B
    - deselection avec clique droit
    - bouton d'annulation la selection

--- components/pokedexToolBar.vue
+++ components/pokedexToolBar.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="flex flex-wrap gap-2 justify-center mb-3 items-center">
-    <button @click="$emit('update:selectionMode', !selectionMode)" :class="selectionMode ? 'bg-slate-800 text-white border-slate-800' : 'bg-white text-slate-800 border-slate-300'"
-    class="px-3 py-1 text text-sm border rounded hover:bg-slate-200 hover:text-black transition-colors">
-      {{ selectionMode ? 'Quitter la sélection' : 'Sélectionner'}}
-    </button>
-    <span v-if="selectionMode && selectedPokemons.length > 0" class="text-sm text-slate-600">
-      {{ selectedPokemons.length }} sélectionné(s)
-    </span>
-  </div>
+  <div class="sticky top-0 max-h-screen w-40 flex flex-col gap-10 py-20 bg-slate-200/50 overflow-y-auto">
+    <div class="flex flex-col gap-4 justify-center mb-3 items-center">
 
-  <div v-if="selectionMode" class="flex flex-wrap gap-2 justify-center mb-3 items-center">
-    <button @click="empty" class="px-3 py-1 text text-sm border rounded hover:bg-slate-200 transition-colors">Non Possédé</button>
-    <button @click="normal" class="px-3 py-1 text text-sm border rounded hover:bg-slate-200 transition-colors">Possédé</button>
-    <button @click="shiny" class="px-3 py-1 text text-sm border rounded hover:bg-slate-200 transition-colors">Shiny</button>
+      <label>Selectors</label>
+
+      <button title="Activer la sélection unitaire" @click="$emit('update:selectionMode', !selectionMode)" :class="selectionMode ? 'bg-slate-800 text-white border-slate-800' : 'bg-white text-slate-800 border-slate-300'"
+              class="w-10 h-10 rounded-full flex items-center justify-center text-xs text-center leading-tight border px-3 py-1 text text-sm border rounded-full hover:bg-slate-200 hover:text-black transition-colors">
+        1
+      </button>
+
+      <button title="Activer la sélection d'ensemble" @click="$emit('update:rangeSelectionMode', !rangeSelectionMode)" :class="rangeSelectionMode ? 'bg-slate-800 text-white border-slate-800' : 'bg-white text-slate-800 border-slate-300'"
+              class="w-10 h-10 rounded-full flex items-center justify-center text-xs text-center leading-tight borderpx-3 py-1 text text-sm border rounded-full hover:bg-slate-200 hover:text-black transition-colors">
+        A-B
+      </button>
+
+      <button title="Annuler la sélection" :disabled="!selectionMode && !rangeSelectionMode" @click="$emit('update:selectionMode', false); $emit('update:rangeSelectionMode', false)" class="disabled:opacity-50 disabled:cursor-not-allowed w-10 h-10 rounded-full flex items-center justify-center text-xs text-center leading-tight border transition-colors bg-red-300 text-red-700 border-red-400 px-3 py-1 text text-sm border rounded hover:bg-red-500 transition-colors"><X  /></button>
+
+
+      <span v-if="(selectionMode || rangeSelectionMode) && selectedPokemons.length > 0" class="text-sm text-slate-600">
+        {{ selectedPokemons.length }} sélectionné(s)
+      </span>
+    </div>
+
+    <div class="flex flex-col gap-4 justify-center mb-3 items-center">
+      <label>Statut</label>
+      <button title="Marquer comme non possédé" :disabled="selectedPokemons.length === 0" @click="empty" class="disabled:opacity-50 disabled:cursor-not-allowed w-10 h-10 rounded-full flex items-center justify-center text-xs text-center leading-tight border transition-colors bg-slate-300 text-slate-700 border-slate-400 hover:bg-slate-400"><Minus /></button>
+      <button title="Marquer comme possédé" :disabled="selectedPokemons.length === 0" @click="normal" class="disabled:opacity-50 disabled:cursor-not-allowed w-10 h-10 rounded-full flex items-center justify-center text-xs text-center leading-tight border transition-colors bg-blue-400 text-white border-blue-500 hover:bg-blue-500"><Plus /></button>
+      <button title="Marquer comme shiny" :disabled="selectedPokemons.length === 0" @click="shiny" class="disabled:opacity-50 disabled:cursor-not-allowed w-10 h-10 rounded-full flex items-center justify-center text-xs text-center leading-tight border transition-colors bg-yellow-400 text-white border-yellow-500 hover:bg-yellow-500"><Star class="w-5 h-5 fill-current" /></button>
+    </div>
   </div>
 </template>
 
@@ -27,10 +42,12 @@ import {useAuth} from "~/composables/useAuth";
 const props = defineProps<{
   selectedPokemons: number[]
   selectionMode: boolean
+  rangeSelectionMode: boolean
 }>()
 
 const emit = defineEmits<{
   'update:selectionMode': [value: boolean]
+  'update:rangeSelectionMode': [value: boolean]
   saved: []
 }>()
 
@@ -47,8 +64,12 @@ async function empty() {
       }
     })
   }
-
-  emit('update:selectionMode', false)
+  if(props.selectionMode){
+    emit('update:selectionMode', false)
+  }
+  else{
+    emit('update:rangeSelectionMode', false)
+  }
   emit('saved')
 }
 
@@ -62,8 +83,12 @@ async function normal() {
       }
     })
   }
-
-  emit('update:selectionMode', false)
+  if(props.selectionMode){
+    emit('update:selectionMode', false)
+  }
+  else{
+    emit('update:rangeSelectionMode', false)
+  }
   emit('saved')
 }
 
@@ -77,9 +102,15 @@ async function shiny() {
       }
     })
   }
-
-  emit('update:selectionMode', false)
+  if(props.selectionMode){
+    emit('update:selectionMode', false)
+  }
+  else{
+    emit('update:rangeSelectionMode', false)
+  }
   emit('saved')
 }
+
+import { Plus, Minus, Star, X } from 'lucide-vue-next'
 
 </script>
--- components/pokemonCard.vue
+++ components/pokemonCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-36 flex flex-col items-center bg-white border rounded-md p-2"
-    :class="[pokemon.isShiny ? 'border-yellow-300 hover:border-yellow-500' : (pokemon.isOwned ? 'border-blue-300 hover:border-blue-500' : 'border-slate-400 hover:border-slate-600'),
-             selected ? 'border-[4px]' : 'border-[2px]']">
+       :class="[rangeStartEnd ? 'border-red-500' : pokemon.isShiny ? 'border-yellow-300 hover:border-yellow-500' : pokemon.isOwned ? 'border-blue-400 hover:border-blue-500' : 'border-slate-400 hover:border-slate-600',
+    rangeStartEnd ? 'border-[3px]' : selected ? 'border-[3px]' : 'border-[2px]']">
     <div class="relative flex justify-between w-full">
       <span class="text-[10px] text-slate-400 self-start">#{{ pokemon.pokeNumber }}</span>
       <span v-if="pokemon.isShiny" class="absolute top-0 right-0 text-[20px] -m-[3px] text-yellow-400">&#9733;</span>
@@ -9,8 +9,8 @@
     <div v-if="!imageLoader" class="w-full aspect-square bg-slate-100 rounded">
       <img src="https://i.gifer.com/4OKl.gif">
     </div>
-    <img  ref="spriteEl" :src="pokemon.isShiny && pokemon.currentSprite ? shinyUrl(pokemon.currentSprite) ?? undefined : pokemon.currentSprite ?? undefined" :class= "pokemon.isShiny || pokemon.isOwned ? 'w-full aspect-square object-contain' : 'w-full aspect-square object-contain grayscale'" loading="lazy" @load="onSpriteLoaded"/>
-    <p class="mt-1 text-xs w-full text-center truncate">{{ pokemon.nameFr }}</p>
+    <img ref="spriteEl" :src="pokemon.isShiny && pokemon.currentSprite ? shinyUrl(pokemon.currentSprite) ?? undefined : pokemon.currentSprite ?? undefined" :class= "imageLoader ? (pokemon.isShiny || pokemon.isOwned ? 'w-full aspect-square object-contain' : 'w-full aspect-square object-contain grayscale') : 'w-0 h-0 overflow-hidden opacity-0'" loading="lazy" @load="onSpriteLoaded"/>
+    <p class="mt-1 text-xs w-full text-center truncate" :class="imageLoader ? '' : 'opacity-0'" >{{ pokemon.nameFr }}</p>
   </div>
 </template>
 
@@ -19,6 +19,8 @@
 </style>
 
 <script setup lang="ts">
+import {shinyUrl} from "#server/utils/sprite";
+
 const props = defineProps<{
   pokemon: {
     id: number
@@ -29,6 +31,7 @@ const props = defineProps<{
     isOwned: boolean
   }
   selected?: boolean
+  rangeStartEnd?: boolean
 }>()
 
 const imageLoader = ref(false)
@@ -36,14 +39,6 @@ const spriteEl = ref<HTMLImageElement>()
 
 function onSpriteLoaded() {
   imageLoader.value = true
-}
-
-function shinyUrl(url: string) {
-  if(url.includes('red-blue') || url.includes('yellow')){
-    return url
-  }
-  const partie = url.split('//')
-  return partie[0] + '//' + partie[1] + '/shiny/' + partie[2]
 }
 
 onMounted(() => {
