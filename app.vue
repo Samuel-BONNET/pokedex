5b045c279f982e5b89fae274578971c513a0fc59
twist <s.bonnet230906@gmail.com>
2026-09-08 21:45:32 +0200

    ⌛ feat & fix: Loading screen
    + Add loading screen si temps chargement > 1 sec
    + Minor upgrade tier list
    + Fix lien export CSV
    + Fix docker build

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
 <template>
+  <LoadingPage :loading="loading" />
+
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+const loading = ref(false)
+
+const router = useRouter()
+
+router.beforeEach(() => {
+  loading.value = true
+})
+
+router.afterEach(() => {
+  loading.value = false
+})
+</script>
