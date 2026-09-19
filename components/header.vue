bd4716784ec51d064d0b74069c01a8b214e76441
twist <s.bonnet230906@gmail.com>
2026-09-19 19:25:07 +0200

    🏷️ evol: Upgrade Header
    + 2 version ( hub & rest )

--- components/header.vue
+++ components/header.vue
@@ -1,16 +1,30 @@
 <template>
-  <div class="flex flex-col items-center justify-center">
-    <NuxtLink to="/">
-      <img class="max-w-xl" src="/logo.png" />
-    </NuxtLink>
+  <div v-if="route.path === '/'">
+    <div class="flex flex-col items-center justify-center">
+      <NuxtLink to="/">
+        <img class="max-w-xl" src="/logo.png" />
+      </NuxtLink>
+    </div>
+    <div class="flex flex-row items-center justify-center">
+      <Navbar />
+      <Login />
+    </div>
   </div>
-  <div class="flex flex-row items-center justify-center">
-    <Login />
-    <Navbar />
+  <div v-else>
+    <div class="flex flex-row items-center justify-between">
+      <div class="flex flex-row items-center gap-4">
+        <NuxtLink to="/">
+          <img class="max-w-xs m-4" src="/logo.png" />
+        </NuxtLink>
+        <Navbar />
+      </div>
+      <Login />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+const route = useRoute()
 
 </script>
 
